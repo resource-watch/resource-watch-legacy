bc3b42c013c730efd33a0fa8d7a8d3b9dce2add1
Gerardo <gerardo.pacheco@vizzuality.com>
2016-04-01 12:07:02 +0200

    Wip: Chart selector

--- app/assets/javascripts/views/chart_selector_view.js
+++ app/assets/javascripts/views/chart_selector_view.js
@@ -8,9 +8,14 @@
 
     className: 'chart-selector',
 
+    events: {
+      'change .type': '_onTypeChange'
+    },
+
     template: this.HandlebarsTemplates.chart_selector,
 
     props: {
+      elSelectsValues: '.value',
       chartConfig: [
         {
           name: 'bar',
@@ -31,15 +36,37 @@
         {
           name: 'pie',
           acceptedStatTypes: [
-            [ 'nominal' ],
-            [ 'ordinal' ]
+            [ 'quantitative' ]
           ]
         }
-      ]
+      ],
+      chartsTypes: {
+        bar: [{
+          label: 'X axis',
+          name: 'x'
+        },
+        {
+          label: 'Y axis',
+          name: 'y'
+        }],
+        line: [{
+          label: 'X axis',
+          name: 'x'
+        },
+        {
+          label: 'Y axis',
+          name: 'y'
+        }],
+        pie: [{
+          label: 'Value',
+          name: 'value'
+        }]
+      }
     },
 
     state: {
-      data: null
+      data: null,
+      type: null
     },
 
     initialize: function(settings) {
@@ -47,25 +74,87 @@
         this.state.set({
           data: settings.data
         }, { silent: true });
-        this.render();
+        this._getChartsSuggestions();
       }
     },
 
     render: function() {
-      var data = this._getChartsSuggestions();
-      this.$el.html(this.template(data));
+      var recomms = [];
+      var columns = this._getColumns();
+
+      _.each(this.data.recomms, _.bind(function(d) {
+        recomms.push({
+          type: d,
+          selected: this.state.attributes.type === d
+        })
+      }, this));
+
+      this.$el.html(this.template({
+        recomms: recomms,
+        columns: columns
+      }));
+      this._generateGraph();
     },
 
     _getChartsSuggestions: function() {
-      var jiminy = new Jiminy(this.state.attributes.data,
+      this.jiminy = new Jiminy(this.state.attributes.data,
         this.props.chartConfig);
-      var recomm = jiminy.recommendation();
-      var columns = jiminy.columns('bar');
+      var columns = [];
+      var recomm = this.jiminy.recommendation();
 
-      return {
+      if (recomm) {
+        columns = this.jiminy.columns(recomm[0]);
+        this.state.set({
+          type: recomm[0]
+        });
+      }
+
+      this.data = {
         recomms: recomm,
         columns: columns
       };
+      this.render();
+    },
+
+    _generateGraph: function() {
+      var $values = this.el.querySelectorAll(this.props.elSelectsValues);
+      var selectedColumns = [];
+      for (var i = 0; i < $values.length; i++) {
+        var current = $values[i];
+        var selected = current.querySelector('option:checked');
+        selectedColumns.push(selected.value);
+      }
+
+      var newDataSet = _.pluck(this.state.attributes, selectedColumns);
+      console.log(selectedColumns, newDataSet);
+    },
+
+    _getColumns: function() {
+      var chartTypes = this.props.chartsTypes[this.state.attributes.type];
+      var chartColumns = [];
+
+      _.each(chartTypes, _.bind(function(d, i) {
+        var column = {
+          label: d,
+          options: _.map(this.data.columns, function(c, e) {
+            return {
+              column: c,
+              selected: i === e
+            };
+          })
+        };
+        chartColumns.push(column);
+      }, this));
+      return chartColumns;
+    },
+
+    _onTypeChange: function(ev) {
+      var current = ev.currentTarget;
+      var type = current.value;
+
+      this.state.set({
+        type: type
+      });
     }
   });
 
